8efcde8caccfd4a4066b273dccada9926cab6a1d
Mike Fogel <mike@fogel.ca>
2010-08-19 19:44:38 -0700

    setup.py, half done README

--- README.txt
+++ README.txt
@@ -1,1 +1,22 @@
+django-settings-context-processor
+---------------------------------
+
+
+What does it do?
+---------------
 Makes specified django settings visible in template rendering context.
+
+
+How does it do it?
+------------------
+It takes the setting variable TEMPLATE_VISIBLE_SETTINGS, which should
+be an iterable of strings, and looks for a settings of that name.  If it
+finds one, it places it in the template rendering context.
+
+
+Can I see an example?
+---------------------
+Yup, here's the key parts of a settings.py:
+
+TODO: fill me in.
+
---
+++ setup.py
@@ -0,0 +1,21 @@
+
+from distutils.core import setup
+
+setup(
+    name='django-settings-context-processor',
+    version='0.1',
+    description='Makes specified django settings visible in template rendering context.',
+    author='Mike Fogel',
+    author_email='mike@fogel.ca',
+    url='http://github.com/carbonXT/django-settings-context-processor',
+    packages=['settings-context-processor'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
+) 
